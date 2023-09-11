bcbd5904319756500b78123ea0c7f170675ace4f
Jose Mauricio <grandemau@gmail.com>
2023-09-11 16:00:48 -0300

    Adicionado icones nas tabs

--- src/routes/Tab.js
+++ src/routes/Tab.js
@@ -3,11 +3,40 @@ import TelaA from '../views/TelaA'
 import TelaB from '../views/TelaB'
 import TelaC from '../views/TelaC'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const Tab = createBottomTabNavigator()
 
 export default props => (
-    <Tab.Navigator initialRouteName='TelaA'>
+    <Tab.Navigator initialRouteName='TelaA'
+        screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+                let iconName;
+
+                switch (route.name) {
+                    case "TelaA":
+                        iconName = focused
+                            ? 'list'
+                            : 'list-ul';
+                        break;
+                    case "TelaB":
+                        iconName = focused
+                            ? 'question'
+                            : 'question-circle';
+                        break;
+                    case "TelaC":
+                        iconName = focused
+                            ? 'exclamation'
+                            : 'exclamation-circle';
+                        break;
+                }
+                // You can return any component that you like here!
+                return <Icon name={iconName} size={size} color={color} />;
+            },
+            tabBarActiveTintColor: 'tomato',
+            tabBarInactiveTintColor: 'gray',
+        })}
+    >
         <Tab.Screen name='TelaA' component={TelaA} />
         <Tab.Screen name='TelaB' component={TelaB} />
         <Tab.Screen name='TelaC' component={TelaC} />
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@
 import React from 'react'
 import { SafeAreaView, Text } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
-// import Stack from './Stack'
+import Stack from './Stack'
 import Tab from './Tab'
 // import Drawer from './Drawer'
 
